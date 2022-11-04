7d2187413d85ac7e6aef7a15ad8964737f8d9ea2
sihyun2988 <71647537+sihyun2988@users.noreply.github.com>
2022-11-04 13:12:14 +0900

    4 sh

---
+++ src/shell/ex.3-4.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "리눅스가 재미있나요? (yes/no)"
+read answer
+case $answer in
+  yes | y | Yes | Y | YES)
+    echo "yes";;
+  [nN]*)
+    echo "no";;
+  *)
+    echo "yes or no로 입력하세요."
+    exit 1;;
+esac
+exit 0
+
